8acf7541f0b102734f39040147a35d92162fe87e
richardmazo <richard.mazo.97@hotmail.com>
2023-11-03 17:28:23 -0500

    Terminando modulo

---
+++ 06-heroesApp/src/app/auth/guards/public.guard.ts
@@ -0,0 +1,51 @@
+//se importa esta libreria para poder inyectar dependencias sin constructor de clase
+import { inject } from '@angular/core';
+import { Observable, map, tap } from 'rxjs';
+import {
+  ActivatedRouteSnapshot,
+  CanActivateFn,
+  CanMatchFn,
+  Route,
+  Router,
+  RouterStateSnapshot,
+  UrlSegment,
+} from '@angular/router';
+import { AuthService } from '../services/auth.service';
+
+const checkAuthStatus = (): boolean | Observable<boolean> => {
+  //se inyectan el AuthService y el Router
+  const authService: AuthService = inject(AuthService);
+  const router: Router = inject(Router);
+
+  return authService.checkAuthentication().pipe(
+    tap((isAuthenticated) => {
+      if (isAuthenticated) {
+        router.navigate(['./heroes/list']);
+      }
+    }),
+    map( isAuthenticated => !isAuthenticated )
+  );
+};
+
+//No hay necesidad de crear una clase, simplemente definiendo una función flecha y exportándola podemos utilizar sus funcionalidades de guard en el app-routing
+export const canActivatePublicGuard: CanActivateFn = (
+  //Hay que tener en cuenta el tipado CanActiveFn
+  route: ActivatedRouteSnapshot,
+  state: RouterStateSnapshot
+) => {
+  //console.log('CanActivate');
+  //console.log({ route, state });
+
+  return checkAuthStatus();
+};
+
+export const canMatchPublicGuard: CanMatchFn = (
+  //Tipado CanMatchFN
+  route: Route,
+  segments: UrlSegment[]
+) => {
+  //console.log('CanMatch');
+  //console.log({ route, segments });
+
+  return checkAuthStatus();
+};
